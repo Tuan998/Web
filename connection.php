c23233e44cba65c2b89f5dac0e55426ba844a1ae
Tuan998 <100896109+Tuan998@users.noreply.github.com>
2022-06-27 15:19:58 +0700

    Update con.php
    6/27/2022

--- connection.php
+++ connection.php
@@ -1,5 +1,5 @@
 <?php
-$conn = pg_connect("postgres://zmwunuawhfncoa:3cc4307784e2497e90c925bf9b032ab947aa347939d70d422f621668d9ae2827@ec2-35-169-49-157.compute-1.amazonaws.com:5432/d2jvl120enokg6");
+$conn = pg_connect("postgres://dlptpimddubbtw:1ce6245fa9d941030db5ecccb64c39d2457f621d029f2585b7da5d5dbd791c42@ec2-44-205-41-76.compute-1.amazonaws.com:5432/d64l5gp4nd5e3s");
 	echo 'Connected Successfully!!!';
 	if(!$conn)
 	{
